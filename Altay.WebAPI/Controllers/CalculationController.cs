feb300721a53fc160fd663993e90692493a53b82
Omer Akdag <a.omer@baylanwatermeters.com>
2023-03-15 16:01:44 +0300

    Minor Changes at View and Controller
    Minor Changes at View and Controller

--- Altay.WebAPI/Controllers/CalculationController.cs
+++ Altay.WebAPI/Controllers/CalculationController.cs
@@ -1,36 +1,39 @@
 ﻿using Altay.Business.Managements;
 using Altay.Entities.Models.Result;
+using Altay.WebAPI.Pages;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Altay.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CalculationController : ControllerBase
+    public class CalculationController : Controller
     {
         [HttpPost("CalculateClosestPrimeNumber")]
-        public ResultObject<int> CalculateClosestPrimeNumber(string numberString)
+        public IActionResult CalculateClosestPrimeNumber([FromForm] string NumberString)
         {
             ResultObject<int> resultObject = new ResultObject<int>();
-
+            
             if (!ModelState.IsValid) 
             {
-                return resultObject;   
+                return Redirect("~/Index");
             }
-
             
             CalculationManagement calculationMan = new CalculationManagement();
 
-            var numberList = numberString.Split(',').Select(c => Convert.ToInt32(c)).ToList();
+            var numberList = NumberString.Split(',').Select(c => Convert.ToInt32(c)).ToList();
             
             resultObject = calculationMan.CalculateClosestPrimeNumber(numberList);
             resultObject.ResultCode = Entities.Enums.enGeneral.Results.OK;
-            return resultObject;
+            TempData["result"] = resultObject.Object;
+            TempData["inputText"] = NumberString;
+            return Redirect("~/Index");
         }
     }
 }
